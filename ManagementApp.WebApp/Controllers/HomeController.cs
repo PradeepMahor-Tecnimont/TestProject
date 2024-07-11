0e7ab727e13184dafd992a9b816bd06294bb6afc
Mahor Pradeepkumar (Mumbai - IT) <p.mahor@tecnimont.in>
2024-07-11 09:55:02 +0530

    implement NiceAdmin UI

--- ManagementApp.WebApp/Controllers/HomeController.cs
+++ ManagementApp.WebApp/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace MGMTApp.WebApp.Controllers
             return View();
         }
 
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
